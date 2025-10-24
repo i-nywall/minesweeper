7fb0ee206deae0f2e7b7267f952b883de1d21aee
Isak Nywall <nywall@pm.me>
2025-10-24 13:26:41 +0200

    feat: make grid responsive and work with differents row/column amounts

--- scripts/main.js
+++ scripts/main.js
@@ -1,5 +1,5 @@
 const gameSettings = {
-  rows: 10,
+  rows: 30,
   columns: 10,
   mines: 10,
 };
@@ -12,12 +12,21 @@ function fillBoard() {
   for (let row = 0; row < gameSettings.rows; row++) {
     for (let col = 0; col < gameSettings.columns; col++) {
       const cellElement = document.createElement("div");
+      cellElement.dataset.row = row;
+      cellElement.dataset.col = col;
+      cellElement.dataset.state = "hidden";
+      cellElement.dataset.flagged = "false";
       cellElement.classList.add("cell");
       fragment.appendChild(cellElement);
     }
   }
 
   game.appendChild(fragment);
+  document.documentElement.style.setProperty("--board-rows", gameSettings.rows);
+  document.documentElement.style.setProperty(
+    "--board-columns",
+    gameSettings.columns,
+  );
 }
 
 fillBoard();
